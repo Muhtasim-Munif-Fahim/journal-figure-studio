dc514410898a7c0941d98eab593f5b27b7812f70
Muhtasim-Munif-Fahim <s1911024120@ru.ac.bd>
2026-06-26 02:32:15 +0600

    refactor: extract constants module with shared config values

---
+++ scripts/constants.py
@@ -0,0 +1,7 @@
+"""Configuration constants for journal-figure-studio."""
+
+MIN_RASTER_DPI = 300
+MIN_FONT_PT = 7
+DEFAULT_ASPECT_RATIO = 0.68
+MIN_PNG_WIDTH_INCHES = 1.0
+SUPPORTED_FORMATS = ["pdf", "png", "tiff", "svg"]
